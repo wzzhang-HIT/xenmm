ee2727bca931a1461290dbddf7d34a8494f882e6
xiehuc <xiehuc@gmail.com>
2012-12-25 11:29:26 +0800

    add error description
    Signed-off-by: xiehuc <xiehuc@gmail.com>

--- src/mmserver.c
+++ src/mmserver.c
@@ -1,3 +1,13 @@
+/*=============================================================================
+#     FileName: mmserver.c
+#         Desc: the main file of memory manage server 
+#       Author: xiehuc
+#        Email: xiehuc@gmail.com
+#     HomePage: 
+#      Version: 0.0.1
+#   LastChange: 2012-12-22 19:35:07
+#      History:
+=============================================================================*/
 #include "type.h"
 #include "mmstore.h"
 
@@ -12,6 +22,15 @@ int main()
     }
     s_h_list_domains();
     Domain* domainu;
+    if(LIST_EMPTY(&domain0.domainu)){
+        fprintf(stderr,"it seems you didn't run mmclient in any guest vm.\n"
+                "you can install mmclient in your guest vms.\n"
+                "and use `sudo services mmclientd start`\n"
+                "or `sudo /etc/init.d/mmclientd start` to run mmclient.\n"
+               );
+        s_h_close();
+        return;
+    }
     LIST_FOREACH(domainu,&domain0.domainu,entries){
         s_h_watch_guest_mem(domainu, domainu_mem_change, domainu);
     }
