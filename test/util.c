52a40476ae5a113299dd1cfee9b3f9828c219229
xiehuc <xiehuc@gmail.com>
2013-01-09 14:07:27 +0800

    add swap record Signed-off-by: xiehuc <xiehuc@gmail.com>
    Signed-off-by: xiehuc <xiehuc@gmail.com>

--- test/util.c
+++ test/util.c
@@ -3,6 +3,62 @@
 #include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
+#include <time.h>
+
+#if RECORD_SWAP
+typedef unsigned long memkb_t;
+struct SwapInfo{
+    memkb_t total;
+    memkb_t freed;
+};
+static int record_fd = -1;
+static int proc_fd = -1;
+static void record_swap()
+{
+    //reduce write times
+    static int reduce = 0;
+    reduce %= 3;
+    if(reduce++!=0) return;
+
+    char buf[4096];
+    struct SwapInfo info = {0};
+    char* ptr;
+    time_t t = time(NULL);
+    struct tm *tm_ = localtime(&t);
+    if(record_fd==-1){
+        snprintf(buf,sizeof(buf),"/proc/meminfo");
+        proc_fd = open(buf,O_RDONLY);
+        strftime(buf, sizeof(buf), "[swap]%y-%m-%d-%X.log", tm_);
+        record_fd = open(buf,O_WRONLY|O_CREAT);
+        snprintf(buf,sizeof(buf),"record in kb\ntime\t\ttotal\t\tused\t\tfree\n");
+        write(record_fd,buf,strlen(buf));
+    }
+    lseek(proc_fd,0,SEEK_SET);
+    size_t len = read(proc_fd,buf,sizeof(buf)-1);
+    buf[len]='\0';
+    ptr = strstr(buf,"SwapTotal:");
+    ptr+=strlen("SwapTotal:");
+    info.total = strtoul(ptr, NULL, 10);
+    ptr = strstr(ptr,"SwapFree:");
+    ptr+=strlen("SwapFree:");
+    info.freed = strtoul(ptr, NULL, 10);
+    strftime(buf,sizeof(buf),"[%X]\t\t",tm_);
+    snprintf(buf+strlen(buf),sizeof(buf)-strlen(buf),"%lu\t\t%lu\t\t%lu\n",info.total,info.total-info.freed,info.freed);
+    write(record_fd,buf,strlen(buf));
+}
+static void close_swap()
+{
+    close(record_fd);
+    close(proc_fd);
+    record_fd = proc_fd = -1;
+}
+#else
+//empty define
+#define record_swap()
+#define close_swap()
+#endif
+
+
 
 double rand_num()
 {
@@ -61,6 +117,7 @@ void flush_pages()
         for(i=0;i<page_num;i++){
             memset(trunks[i],times,ONE_PAGE);
         }
+        record_swap();
     }
 }
 #else
@@ -73,6 +130,7 @@ void flush_pages()
             read(trunks[i], ONE_PAGE, rand_stream);
         }
     }
+    record_swap();
     close(rand_stream);
 }
 #endif
@@ -93,4 +151,5 @@ void free_all_pages()
         free(trunks[i]);
     }
     page_num = 0;
+    close_swap();
 }
