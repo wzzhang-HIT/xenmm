0592a4e0de04e1526f89f81efaec92900f96317c
xiehuc <xiehuc@gmail.com>
2013-02-19 18:52:21 +0800

    update client
    Signed-off-by: xiehuc <xiehuc@gmail.com>

--- src/mmclient.c
+++ src/mmclient.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <string.h>
 #include "type.h"
 #include "mmstore.h"
 
@@ -10,6 +13,7 @@ int main()
     if(s_g_init())
         return 0;
 
+    /*
     ulong pagesize = sysconf(_SC_PAGESIZE)/1024;//kb
     while(1){
         ulong tot = sysconf(_SC_PHYS_PAGES)*pagesize;
@@ -18,6 +22,27 @@ int main()
         mem.free_mem = free;
         printf("tot:%lu,free:%lu\n",tot,free);
         s_g_write_mem(mem);
+        sleep(1);
+    }*/
+
+    char buf[4096];
+    char* ptr;
+    while(1){
+        int fd = open("/proc/meminfo",O_RDONLY);
+        size_t len = read(fd,buf,sizeof(buf)-1);
+        buf[len]='\0';
+        ptr = strstr(buf,"MemTotal:")+strlen("MemTotal:");
+        mem.tot_mem = strtoull(ptr,NULL,10);
+        ptr = strstr(ptr,"MemFree:")+strlen("MemFree:");
+        unsigned long long free = strtoull(ptr,NULL,10);
+        //ptr = strstr(ptr,"Buffers:")+strlen("Buffers:");
+        //unsigned long long buffer = strtoull(ptr,NULL,10);
+        ptr = strstr(ptr,"Cached:")+strlen("Cached");
+        unsigned long long cached = strtoull(ptr,NULL,10);
+        mem.free_mem = free+cached;
+        printf("tot:%llu,free:%llu\n",mem.tot_mem,mem.free_mem);
+        s_g_write_mem(mem);
+        close(fd);
         sleep(1);
     }
 
